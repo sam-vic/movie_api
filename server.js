d2faf5a205f846448ccf975f8ad4d10ed39659fa
SamLCH <64434536+sam-vic@users.noreply.github.com>
2023-05-20 23:19:19 -0700

    doc: added api for post new user, put user info, and delete

--- server.js
+++ server.js
@@ -44,25 +44,56 @@ app.get("/movies", (req, res) => {
   })
 });
 //Movie based on title
-app.get("/movies/:title", (req, res) => {
-  res.json(
-    data.movies.find((movie) => {
-      return movie.title === req.params.title;
-    })
-  );
-});
+app.get("/movies/:Title", (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+  .then((movies) => {
+    res.status(201).json(movies)
+  })
+  .catch((err) => {
+    console.error(err)
+    res.status(500).send("Error:" + err)
+  })
+})
 
-app.get("/genre/:name", (req, res) => {
-  const genreName = req.params.name;
-  const genreMovies = data.movies.filter((movie) => movie.genre === genreName);
-  if (genreMovies.length === 0) {
-    return res.status(404).send("No match");
-  } else if (genreMovies.length === 1) {
-    return res.json(genreMovies[0]);
-  } else {
-    return res.json(genreMovies);
-  }
-});
+//Movie based on genre
+app.get("/movies/:genreName", (req, res) => {
+  Movies.findOne({ 'Genre.Name': req.params.genreName })
+  .then((movies) => {
+    res.status(201).json(movies)
+  })
+  .catch((err) => {
+    console.error(err)
+    res.status(500).send("Error:" + err)
+  })
+})
+
+//Get User list
+app.get("/users", (req, res) => {
+  Users.find()
+  .then((users) => {
+    res.status(201).json(users)
+  })
+  .catch((err) => {
+    console.error(err)
+    res.status(500).send("Error:" + err)
+  })
+})
+
+//Get User by username
+app.get("/users/:Username", (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+  .then((users) => {
+    if(!users) {
+      return res.status(400).send(" Unable to find_" + req.body.Username);
+    } else {
+      res.status(201).json(users)
+    }
+  })
+  .catch((err) => {
+    console.error(err)
+    res.status(500).send("Error:" + err)
+  })
+})
 
 //Add a user
 /* We’ll expect JSON in this format
@@ -83,7 +114,7 @@ app.post("/users", (req, res) => {
           Username: req.body.Username,
           Password: req.body.Password,
           Email: req.body.Email,
-          Birthday: req.body.Birthday,
+          Birthday: req.body.Birthday
         })
           .then((user) => {
             res.status(201).json(user);
@@ -97,6 +128,42 @@ app.post("/users", (req, res) => {
     .catch((error) => {
       console.error(error);
       res.status(500).send("Error: " + error);
+    });
+})
+//Update user
+app.put('/users/:Username', async (req, res) => {
+  try {
+    const updatedUser = await Users.findOneAndUpdate(
+      { Username: req.params.Username },
+      {
+        $set: {
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        }
+      },
+      { new: true }
+    );
+    res.json(updatedUser)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error: ' + error)
+  }
+})
+
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     });
 });
 
