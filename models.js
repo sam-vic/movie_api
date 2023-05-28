09330deea34be3f7e034ee58b59a5915683e0d56
SamLCH <64434536+sam-vic@users.noreply.github.com>
2023-05-28 14:11:17 -0700

    doc: adding password hashing and hashing validation

--- models.js
+++ models.js
@@ -23,7 +23,16 @@ let movieSchema = mongoose.Schema({
     Email: {type: String, required: true},
     Birthday: Date,
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-  });
+  })
+
+  userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+  };
+  
+  userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+  };
+  
   
   let Movie = mongoose.model('Movie', movieSchema);
   let User = mongoose.model('User', userSchema);
