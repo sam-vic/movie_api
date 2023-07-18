0d139659be41f124e689742b17307fccee33f943
SamLCH <64434536+sam-vic@users.noreply.github.com>
2023-07-18 13:06:24 -0700

    doc: changing it back to go by id

--- index.js
+++ index.js
@@ -229,23 +229,23 @@ app.post('/users',
 app.post('/users/:Username/favoriteMovies', passport.authenticate('jwt', { session: false }), async (req, res) => {
   try {
     const { Username } = req.params
-    const { movieTitle } = req.body
+    const { movieId } = req.body
 
     const user = await Users.findOne({ Username })
     if (!user) {
       return res.status(404).json({ message: 'User not found' })
     }
 
-    const movie = await Movies.findById(movieTitle)
+    const movie = await Movies.findById(movieId)
     if (!movie) {
       return res.status(404).json({ message: 'Movie not found' })
     }
 
-    if (user.FavoriteMovies.includes(movieTitle)) {
+    if (user.FavoriteMovies.includes(movieId)) {
       return res.status(409).json({ message: 'Movie already in favorites' })
     }
 
-    user.FavoriteMovies.push(movieTitle)
+    user.FavoriteMovies.push(movieId)
     await user.save()
 
     res.status(200).json({ message: 'Movie added to favorites' })
